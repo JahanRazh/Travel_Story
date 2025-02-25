32c4af93632063b723287d66f79f8e3cecc29b8a
JAHAN RAMESH <jahanrazh@gmail.com>
2025-02-25 23:28:27 +0530

    chore: update mongodb dependency to version 6.13.1 and refactor multer import in upload routes

--- BackEnd/routes/uploadRoutes.js
+++ BackEnd/routes/uploadRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const uploadController = require("../controllers/uploadController");
-const upload = require("../utilities/multer");
+const upload = require("../multer");
 
 // Upload routes
 router.post("/image-upload", upload.single("image"), uploadController.uploadImage);
