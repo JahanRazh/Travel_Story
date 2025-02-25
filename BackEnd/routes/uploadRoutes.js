7672fa10de37746df6fb76be836d69f55e35f419
JAHAN RAMESH <jahanrazh@gmail.com>
2025-02-25 22:44:22 +0530

    refactor: move multer configuration to utilities and update upload routes

--- BackEnd/routes/uploadRoutes.js
+++ BackEnd/routes/uploadRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const uploadController = require("../controllers/uploadController");
-const upload = require("../multer");
+const upload = require("../utilities/multer");
 
 // Upload routes
 router.post("/image-upload", upload.single("image"), uploadController.uploadImage);
