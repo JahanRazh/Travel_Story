b93a03b944cdffa7d11ef7bc4a559e0d8af55f89
JAHAN RAMESH <jahanrazh@gmail.com>
2025-02-25 21:53:57 +0530

    feat: add upload routes for image upload and deletion functionality

---
+++ BackEnd/routes/uploadRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const uploadController = require("../controllers/uploadController");
+const upload = require("../multer");
+
+// Upload routes
+router.post("/image-upload", upload.single("image"), uploadController.uploadImage);
+router.delete("/delete-image", uploadController.deleteImage);
+
+module.exports = router;
